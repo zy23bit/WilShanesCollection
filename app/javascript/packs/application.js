0d4bd72125a1987f4f36678baaf6b61e1915e1be
zy23bit <shaneyounglol@outlook.com>
2024-04-19 01:43:27 -0500

    Improved style for home page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,12 +13,3 @@ window.Stimulus = Application.start()
 const context = require.context("../controllers", true, /\.js$/)
 Stimulus.load(definitionsFromContext(context))
 
-function updateTaxAndTotal(addressId) {
-  fetch(`/payments/calculate_tax?address_id=${addressId}`)
-    .then(response => response.json())
-    .then(data => {
-      document.getElementById('tax').innerText = data.tax;
-      document.getElementById('total').innerText = data.total;
-    })
-    .catch(error => console.error('Error updating tax and total:', error));
-}
